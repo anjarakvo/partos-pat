7a0a3c95356666c7fd9dde4e5a545cecc3ab4925
dedenbangkit <mail@dedenbangkit.com>
2024-09-02 14:41:49 +0700

    [#3] Check if git folder is available inside backend container

--- backend/test.sh
+++ backend/test.sh
@@ -18,6 +18,8 @@ coverage combine --rcfile=./.coveragerc
 coverage report -m --rcfile=./.coveragerc
 
 if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
+  echo "Uploading coverage to coveralls"
+  ls -al /app
   coveralls
 fi
 
