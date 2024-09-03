6c5db9969d6c55e679f2e88730150c08357cdbd1
ifirmawan <firmawaneiwan@gmail.com>
2024-09-03 21:14:41 +0700

    [#6] Customize initial fonts, color & antd form

---
+++ frontend/src/app/fonts.js
@@ -0,0 +1,6 @@
+import { Source_Sans_3, Open_Sans } from "next/font/google";
+
+export const sourceSans = Source_Sans_3({
+  subsets: ["latin"],
+});
+export const openSans = Open_Sans({ subsets: ["latin"] });
