16bc0f59cdc7d2e3a46041b94d5f32a48f5e5873
ifirmawan <firmawaneiwan@gmail.com>
2024-09-03 21:16:43 +0700

    [#6] Create auth template page

---
+++ frontend/src/app/[locale]/(auth)/template.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import classNames from "classnames";
+import { sourceSans } from "@/app/fonts";
+
+const AuthTemplate = ({ children }) => {
+  return (
+    <div
+      className={classNames(
+        sourceSans.className,
+        "w-full max-w-9xl h-screen mx-auto flex flex-col md:flex-row gap-0 overflow-y-hidden"
+      )}
+    >
+      <div className="hidden md:block w-full md:w-1/2 lg:w-3/4">
+        <Image
+          width={800}
+          height={600}
+          alt="PARTOS-PAT Background"
+          src="https://placehold.co/1400x1100"
+          className="bg-light-3 object-cover object-left w-full h-screen"
+        />
+      </div>
+      <div className="w-full md:w-1/2 lg:w-1/4 bg-light-1 px-6 py-3 text-dark-10 overflow-y-auto">{children}</div>
+    </div>
+  );
+};
+
+export default AuthTemplate;
