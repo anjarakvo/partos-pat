e06dd7bc445de36b6b9e59bc28859e435f413bc3
ifirmawan <firmawaneiwan@gmail.com>
2024-09-03 21:17:33 +0700

    [#6] Create initial page: Login for redirection purpose

---
+++ frontend/src/app/[locale]/(auth)/login/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Login | PARTOS-PAT",
+  description: "Make power dynamics more visible",
+};
+
+const LoginPage = () => {
+  return <h1>Login page</h1>;
+};
+
+export default LoginPage;
